52e8917cc36d9166b09cf605b02632371f6ddb72
Renee Pinna <renee.pinna@gmail.com>
2023-09-19 22:51:09 -0600

    Add Router to app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,57 @@
+import { useEffect, useState } from 'react'
+import { getDrinks } from '../../apiCalls'
 import './App.scss'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from '../HomePage/HomePage'
+
 
 function App() {
-  return <div></div>
+  const [drinks, setDrinks] = useState([])
+
+
+  async function initializeDrinks() {
+    const data = await getDrinks()
+    setDrinks([data.drinks])
+  }
+
+  useEffect(() => {
+    // initializeDrinks()
+    setDrinks([
+      {
+      "strDrink": "Afterglow",
+      "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/vuquyv1468876052.jpg",
+      "idDrink": "12560"
+      },
+      {
+      "strDrink": "Alice Cocktail",
+      "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/qyqtpv1468876144.jpg",
+      "idDrink": "12562"
+      },
+      {
+      "strDrink": "Aloha Fruit punch",
+      "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/wsyvrt1468876267.jpg",
+      "idDrink": "12862"
+      },
+      {
+      "strDrink": "Apello",
+      "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/uptxtv1468876415.jpg",
+      "idDrink": "15106"
+      },
+      {
+      "strDrink": "Apple Berry Smoothie",
+      "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/xwqvur1468876473.jpg",
+      "idDrink": "12710"
+      },
+      {
+      "strDrink": "Apple Karate",
+      "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/syusvw1468876634.jpg",
+      "idDrink": "12564"
+      }])
+  }, [])
+
+  return <Routes>
+    <Route path='/' element={<HomePage drinks={drinks} />}/>
+  </Routes>
 }
 
 export default App
