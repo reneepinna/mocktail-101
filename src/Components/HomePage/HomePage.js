453d490110abee26c93c5bfbda592d279833a2fe
Renee Pinna <renee.pinna@gmail.com>
2023-09-20 20:24:35 -0600

    Add homepage layout

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -2,26 +2,34 @@ import { useState } from 'react';
 import DrinkContainer from '../DrinkContainer/DrinkContainer';
 import './HomePage.scss';
 import { useEffect } from 'react';
+import banner from './banner.jpeg';
 
-function HomePage({ drinks, favorites, toggleFavorite}) {
-  const [randomDrink, setRandomDrink] = useState({})
-   
-  function getRandomDrink () {
+function HomePage({ drinks, favorites, toggleFavorite }) {
+  const [randomDrink, setRandomDrink] = useState({});
+
+  function getRandomDrink() {
     const index = Math.floor(Math.random() * drinks.length);
-    return drinks[index]
+    return drinks[index];
   }
 
   useEffect(() => {
-    setRandomDrink(getRandomDrink())
-  }, [drinks])
+    setRandomDrink(getRandomDrink());
+  }, [drinks]);
 
   return (
     <div className='homePage'>
       <article className='discover'>
-      {/* {randomDrink && <img className='randomDrinkImg' src={randomDrink.strDrinkThumb} />} */}
+        <img className='banner-img' src={banner} />
+        {/* {randomDrink && <img className='randomDrinkImg' src={randomDrink.strDrinkThumb} />} */}
       </article>
       <h1 className='heading'>Discover</h1>
-      <DrinkContainer drinks={drinks} favorites={favorites} toggleFavorite={toggleFavorite} />
+      <main>
+        <DrinkContainer
+          drinks={drinks}
+          favorites={favorites}
+          toggleFavorite={toggleFavorite}
+        />
+      </main>
     </div>
   );
 }
